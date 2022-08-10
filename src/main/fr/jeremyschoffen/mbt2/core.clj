aafc7cc9f61f2d0f77302d0c8974ff548ea93f8a
Jeremy Schoffen <jschoffen.dev@gmail.com>
2022-08-10 17:57:31 +0200

    Putting together some git utilities.

--- src/main/fr/jeremyschoffen/mbt2/core.clj
+++ src/main/fr/jeremyschoffen/mbt2/core.clj
@@ -1,3 +1,16 @@
-(ns fr.jeremyschoffen.mbt2.core)
+(ns fr.jeremyschoffen.mbt2.core
+  (:require
+    [fr.jeremyschoffen.mbt2.git :as git]))
 
+
+(defn assert-clean-repo [& {:as arg}]
+  (when-not (git/clean-repo? arg)
+    (throw (ex-info "Dirty repo." {}))))
+
+
+(defn tag-release! [{:keys [version] :as arg}]
+  (-> arg
+      (assoc :tag-name version)
+      git/tag!)
+  arg)
 
--- src/main/fr/jeremyschoffen/mbt2/git.clj
+++ src/main/fr/jeremyschoffen/mbt2/git.clj
@@ -1,3 +1,71 @@
 (ns fr.jeremyschoffen.mbt2.git
   (:require
+    [clojure.string :as string]
     [clojure.tools.build.api :as b]))
+
+(def default-min-sha-length 10)
+
+(def status-args ["status" "--porcelain"])
+
+(defn status [& {:as arg}]
+  (-> arg
+      (assoc :git-args status-args)
+      b/git-process))
+
+(defn clean-repo? [& {:as arg}]
+  (if-let [st (status arg)]
+    (-> st
+        string/split-lines
+        count
+        (= 0))
+    true))
+
+(defn make-describe-args [{:keys [prefix min-sha-length]
+                           :or {min-sha-length default-min-sha-length}}]
+  ["describe" "--long"
+   "--match" (str prefix "*.*")
+   (format "--abbrev=%d" min-sha-length)
+   "--dirty=-DIRTY"
+   "--always"])
+
+
+(defn describe [& {:as arg}]
+  (-> arg
+      (assoc :git-args (make-describe-args arg))
+      b/git-process))
+
+
+(defn add! [& {:keys [git-add-paths] :as arg}]
+  (-> arg
+      (assoc :git-args (into ["add"] (map str) git-add-paths))
+      b/git-process))
+
+
+(defn add-all! [& {:as arg}]
+  (-> arg
+      (assoc :git-args ["add" "-A"])
+      b/git-process))
+
+
+(defn commit! [& {:keys [commit-msg] :as arg}]
+  (-> arg
+      (assoc :git-args ["commit" "-m" commit-msg])
+      (b/git-process)))
+
+
+(defn tag! [& {:keys [tag-name tag-msg] :as arg}]
+  (let [tag-cmd (if tag-msg
+                  ["tag" "-a" tag-name "-m" tag-msg]
+                  ["tag" tag-name])]
+    (-> arg
+        (assoc :git-args tag-cmd)
+        b/git-process)))
+
+
+
+(comment
+  (clean-repo?)
+  (make-describe-args {})
+  (describe)
+  (require '[clojure.repl])
+  (clojure.repl/doc b/git-count-revs))
